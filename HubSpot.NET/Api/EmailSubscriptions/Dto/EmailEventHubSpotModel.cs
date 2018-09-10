0f4cd4356d57b06b3522a5ef9ddcda281416bdc5
Paul McDowell <4770241+rezalas@users.noreply.github.com>
2018-09-10 11:23:05 -0500

    Added subscription timeline support, refactored GetRoute to be more robust.

---
+++ HubSpot.NET/Api/EmailSubscriptions/Dto/EmailEventHubSpotModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HubSpot.NET.Api.EmailSubscriptions.Dto
+{
+    public class EmailEventHubSpotModel
+    {
+        [DataMember(Name = "id")]
+        public string Id { get; set; }
+
+        [DataMember(Name = "created")]
+        public long Created { get; set; }
+    }
+}
